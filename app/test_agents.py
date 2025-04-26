e28bd4ed528221c1ce59376449d3e3c850941a10
hxastur <arz.mikhail@gmail.com>
2025-04-26 14:32:09 +0300

    Add code

---
+++ app/test_agents.py
@@ -0,0 +1,105 @@
+from typing import Annotated
+from dotenv import load_dotenv
+import os
+
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain_community.vectorstores import Chroma
+from langchain_core.documents import Document
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_groq import ChatGroq
+
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_KEY")
+model_name = "llama-3.1-8b-instant"
+llm = ChatGroq(temperature=0, model_name=model_name, groq_api_key=GROQ_API_KEY)
+
+
+model_name = "sentence-transformers/all-mpnet-base-v2"
+model_kwargs = {"device": "cpu"}
+encode_kwargs = {"normalize_embeddings": False}
+hf = HuggingFaceEmbeddings(
+    model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
+)
+
+docs = [
+    Document(
+        page_content="Paris is the capital of France.",
+        metadata={
+            "source": "1",
+            "url": "https://example.com/france",
+            "title": "France Facts",
+            "author": "John Doe",
+            "date": "2023-10-01",
+        },
+    ),
+    Document(
+        page_content="The Eiffel Tower is located in Paris.",
+        metadata={
+            "source": "2",
+            "url": "https://example.com/eiffel",
+            "title": "Eiffel Tower Info",
+            "author": "Jane Smith",
+            "date": "2023-10-02",
+        },
+    ),
+    Document(
+        page_content="France is known for its wine and cuisine.",
+        metadata={
+            "source": "3",
+            "url": "https://example.com/french-cuisine",
+            "title": "French Culture",
+            "author": "Pierre Leclerc",
+            "date": "2023-10-03",
+        },
+    ),
+]
+
+db = Chroma.from_documents(
+    collection_name="agents_chroma",
+    documents=docs,
+    embedding=hf,
+)
+
+
+class State(TypedDict):
+    # messages: Annotated[list, add_messages]
+    question: str
+    answer: str
+    documents: list[Document]
+
+
+def retrieve(state: State) -> None:
+    pass
+    # question = state["question"]
+    # documents = []
+    # return {"documents": documents}
+
+
+def generate(state: State) -> None:
+    pass
+    # answer = "paris"
+    # return {"answer": answer}
+
+
+workflow = StateGraph(state_schema=State)
+
+workflow.add_node(node="retrieve", action=retrieve)
+workflow.add_node(node="generate", action=generate)
+
+workflow.add_edge(start_key=START, end_key="retrieve")
+workflow.add_edge(start_key="retrieve", end_key="generate")
+workflow.add_edge(start_key="generate", end_key=END)
+
+workflow.compile()
+
+
+# from fastapi import APIRouter
+
+# router = APIRouter()
+
+# @router.powt()
+# async def get_response():
