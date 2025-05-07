83f11d8ab6e54f5fadca05af6cd17fbb10159cd9
hxastur <arz.mikhail@gmail.com>
2025-05-07 15:15:07 +0300

    Add lm studio setup, RAG retrieve and generate, prompt, chain

--- app/test_agents.py
+++ app/test_agents.py
@@ -11,19 +11,37 @@ from langchain_community.vectorstores import Chroma
 from langchain_core.documents import Document
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
 from langchain import hub
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
-
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.tools.retriever import create_retriever_tool
 from pydantic import BaseModel, Field
 
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_KEY")
-model_name = "llama-3.1-8b-instant"
-llm = ChatGroq(temperature=0, model_name=model_name, groq_api_key=GROQ_API_KEY)
+
+def setup_groq():
+    GROQ_API_KEY = os.getenv("GROQ_KEY")
+    model_name = "llama-3.1-8b-instant"
+    llm = ChatGroq(temperature=0, model_name=model_name, groq_api_key=GROQ_API_KEY)
+    return llm
+
+
+def setup_lm_studio():
+    llm = ChatOpenAI(
+        temperature=0.0,
+        base_url=f"http://{os.getenv('HOST')}:1234/v1",
+        api_key="not-needed",
+    )
+    return llm
+
+
+llm = setup_lm_studio()
 
 
 model_name = "sentence-transformers/all-mpnet-base-v2"
@@ -73,56 +91,68 @@ db = Chroma.from_documents(
 )
 
 retriever = db.as_retriever()
-
-from langchain.tools.retriever import create_retriever_tool
-
 retriever_tool = create_retriever_tool(
     retriever,
-    "retrieve_blog_posts",
-    "Search and return information about Lilian Weng blog posts on LLM agents, prompt engineering, and adversarial attacks on LLMs.",
+    "retrieve_france",
+    "Retrieve documents",
 )
 
-tools = [retriever_tool]
+# tools = [retriever_tool]
+# llm_with_tools = llm.bind_tools(tools)
 
 
 class State(TypedDict):
-    # messages: Annotated[list, add_messages]
     question: str
     answer: str
     documents: list[Document]
 
 
-def chatbot(state: State):
-    answer = {"messages": [llm.invoke(state["question"])]}
-    state["answer"] = answer
-    return state
+prompt = PromptTemplate(
+    input_variables=["question", "documents"],
+    template="""
+    Use the following documents to answer the question.
+
+    Documents:
+    {documents}
+
+    Question: {question}
+
+    Answer:
+    """,
+)
+
+chain = LLMChain(
+    llm=llm,
+    prompt=prompt,
+)
 
 
 def retrieve(state: State) -> None:
-    pass
-    # question = state["question"]
-    # documents = []
-    # return {"documents": documents}
+    question = state["question"]
+    documents = retriever.get_relevant_documents(query=question)
+    return {"documents": documents}
 
 
 def generate(state: State) -> None:
-    pass
-    # answer = "paris"
-    # return {"answer": answer}
+    question = state["question"]
+    documents = state["documents"]
+    answer = chain.invoke(
+        input={
+            "question": question,
+            "documents": "\n\n".join([doc.page_content for doc in documents]),
+        }
+    )
+    return {"answer": answer}
 
 
 workflow = StateGraph(state_schema=State)
 
-workflow.add_node(node="chatbot", action=chatbot)
 workflow.add_node(node="retrieve", action=retrieve)
 workflow.add_node(node="generate", action=generate)
 
-# workflow.add_edge(start_key=START, end_key="retrieve")
-# workflow.add_edge(start_key="retrieve", end_key="generate")
-# workflow.add_edge(start_key="generate", end_key=END)
-
-workflow.add_edge(start_key=START, end_key="chatbot")
-workflow.add_edge(start_key="chatbot", end_key=END)
+workflow.add_edge(start_key=START, end_key="retrieve")
+workflow.add_edge(start_key="retrieve", end_key="generate")
+workflow.add_edge(start_key="generate", end_key=END)
 
 workflow.compile()
 
