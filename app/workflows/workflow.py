d621d7b44355f88ea63f7525ca9d1402e1e548d6
hxastur <arz.mikhail@gmail.com>
2025-05-21 14:27:37 +0300

    Add hotel_assistant, hotel_prompt

--- app/workflows/workflow.py
+++ app/workflows/workflow.py
@@ -1,6 +1,7 @@
 from app.libraries.libs import *
 from app.models.llms import setup_groq, setup_lm_studio
 from app.nodes.state import State
+from app.prompts.agentic_prompts import HOTEL_PROMPT
 
 
 load_dotenv()
@@ -11,7 +12,10 @@ tavily_search_tool = TavilySearch(max_results=2)
 
 
 def hotel_assistant(state: State):
-    pass
+    question = state["messages"][-1]
+    prompt = HOTEL_PROMPT.format(question=question)
+    response = llm.invoke(prompt)
+    return {"hotel": response}
 
 
 def restaurant_assistant(state: State):
